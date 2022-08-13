630f84e7c206637b98d0f9aa7bff64b6140aff83
Michael Spangler <ms@michaelspangler.io>
2022-08-13 06:13:46 -0500

    can render sub textures, but still scuffed

--- main.c
+++ main.c
@@ -15,24 +15,49 @@
 
 #define SCREEN_WIDTH   1024
 #define SCREEN_HEIGHT  786
-#define NUM_GLYPHS 128
 #define MAX(a,b) (((a)>(b))?(a):(b))
 
-struct glyph_info {
-	int x0, y0, x1, y1;	// coords of glyph in the texture atlas
-	int x_off, y_off;   // left & top bearing when rendering
-	int advance;        // x advance when rendering
-} info[NUM_GLYPHS];
+	unsigned int indices[] = {  // note that we start from 0!
+		// snake head
+		0, 1, 3,  // first Triangle
+		1, 2, 3,   // second Triangle
+	};
 
 void render_text(const char* text, float x, float y, float scale);
 GLuint compile_shaders(void);
 
+GLuint textureID;
 GLuint vertex_shader;
 GLuint frag_shader;
 GLuint font_vertex_shader;
 GLuint font_frag_shader;
 GLuint texture_program;
 GLuint font_program;
+GLuint vertex_array_object;
+GLuint vertex_buffer_object;
+GLuint element_buffer_object;
+
+int w = 0;
+int h = 0;
+
+typedef struct Character {
+	int xoffset; // x offset in texture
+	int yoffset; // y offset in texture
+
+	float ax;	// advance.x
+	float ay;	// advance.y
+
+	float bw;	// bitmap.width;
+	float bh;	// bitmap.height;
+
+	float bl;	// bitmap_left;
+	float bt;	// bitmap_top;
+
+	float tx;	// x offset of glyph in texture coordinates
+	float ty;	// y offset of glyph in texture coordinates
+} Character;
+
+Character c[128];
 
 const char *texture_vert_shader = "shaders/texture.vert";
 const char *texture_frag_shader = "shaders/texture.frag";
@@ -46,16 +71,12 @@ typedef struct {
 	float y;
 } VEC2;
 
-/// Holds all state information relevant to a character as loaded using FreeType
-typedef struct {
-	int16_t TextureID; // ID handle of the glyph texture
-	VEC2 Size;      // Size of glyph
-	VEC2 Bearing;   // Offset from baseline to left/top of glyph
-	int16_t Advance;   // Horizontal offset to advance to next glyph
-	struct FT_Bitmap_ *bitmap;
-} Character;
-
-Character Characters[128];
+typedef struct point{
+	GLfloat x;
+	GLfloat y;
+	GLfloat s;
+	GLfloat t;
+} point;
 
 int main() {
 	glfwInit();
@@ -179,10 +200,8 @@ int main() {
 		return -1;
 	}
 
-	FT_Set_Pixel_Sizes(face, 14, 14);
+	FT_Set_Pixel_Sizes(face, 32, 32);
 	FT_GlyphSlot g = face->glyph;
-	int w = 0;
-	int h = 0;
 
 	unsigned int rowh = 0;
 	unsigned int roww = 0;
@@ -206,7 +225,6 @@ int main() {
 	h += rowh;
 
 
-	GLuint textureID;
 	glCreateTextures(GL_TEXTURE_2D, 1, &textureID);
 	glBindTexture(GL_TEXTURE_2D, textureID);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, w, h, 0, GL_RED, GL_UNSIGNED_BYTE, 0);
@@ -218,50 +236,24 @@ int main() {
 	// make textures for ASCII characters 0-128
 	int ox = 0;
 	int oy = 0;
-
-	/* for (int i = 65; i < 66; i++) { */
-	/* 	if (FT_Load_Char(face, i, FT_LOAD_RENDER)) { */
-	/* 		fprintf(stderr, "Loading character %c failed!\n", i); */
-	/* 		continue; */
-	/* 	} */
-
-
-	/* 	if (ox + g->bitmap.width + 1 >= SCREEN_WIDTH) { */
-	/* 		oy += rowh; */
-	/* 		rowh = 0; */
-	/* 		ox = 0; */
-	/* 	} */
-
-	/* 	/1* glTexSubImage2D(GL_TEXTURE_2D, 0, ox, oy, g->bitmap.width, g->bitmap.rows, GL_RGBA, GL_UNSIGNED_BYTE, g->bitmap.buffer); *1/ */
-	/* 	/1* c[i].ax = g->advance.x >> 6; *1/ */
-	/* 	/1* c[i].ay = g->advance.y >> 6; *1/ */
-
-	/* 	/1* c[i].bw = g->bitmap.width; *1/ */
-	/* 	/1* c[i].bh = g->bitmap.rows; *1/ */
-
-	/* 	/1* c[i].bl = g->bitmap_left; *1/ */
-	/* 	/1* c[i].bt = g->bitmap_top; *1/ */
-
-	/* 	/1* c[i].tx = ox / (float)w; *1/ */
-	/* 	/1* c[i].ty = oy / (float)h; *1/ */
-
-	/* 	rowh = MAX(rowh, g->bitmap.rows); */
-	/* 	ox += g->bitmap.width + 1; */
-	/* } */
-
 	float img_height = h;
 	float img_width = w;
 	rowh = 0;
 
-	for (uint8_t c = 32; c <127 ; c++)
+	unsigned int txidx = 0;
+	unsigned int tyidx = 0;
+
+	for (uint8_t i = 32; i <127 ; i++)
 	{
-		if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+		if (FT_Load_Char(face, i, FT_LOAD_RENDER))
 		{
 			printf("ERROR::FREETYTPE: Failed to load Glyph\n");
 			return -1;
 		}
 
 		if (ox + g->bitmap.width + 1 >= img_width) {
+			tyidx++;
+			txidx = 0;
 			oy += rowh;
 			rowh = 0;
 			ox = 0;
@@ -277,42 +269,49 @@ int main() {
 				GL_UNSIGNED_BYTE,
 				face->glyph->bitmap.buffer
 				);
+
+		c[i].xoffset = txidx++;
+		c[i].yoffset = tyidx;
+
+		c[i].ax = g->advance.x >> 6;
+		c[i].ay = g->advance.y >> 6;
+
+		c[i].bw = g->bitmap.width;
+		c[i].bh = g->bitmap.rows;
+
+		c[i].bl = g->bitmap_left;
+		c[i].bt = g->bitmap_top;
+
+		c[i].tx = ox / (float)w;
+		c[i].ty = oy / (float)h;
+
 		rowh = MAX(rowh, g->bitmap.rows);
 		ox += g->bitmap.width + 1;
 	}
-	
+
 
 	/* glBindTexture(GL_TEXTURE_2D, 0); */
 
-	/* FT_Done_Face(face); */
-	/* FT_Done_FreeType(ft); */
+	FT_Done_Face(face);
+	FT_Done_FreeType(ft);
 
 	// configure VAO/VBO for texture quads
 	// -----------------------------------
-	/* glGenVertexArrays(1, &VAO); */
-	/* glGenBuffers(1, &VBO); */
-	/* glBindVertexArray(VAO); */
-	/* glBindBuffer(GL_ARRAY_BUFFER, VBO); */
-	/* glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW); */
-	/* glEnableVertexAttribArray(0); */
-	/* glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0); */
-	/* glBindBuffer(GL_ARRAY_BUFFER, 0); */
-	/* glBindVertexArray(0); */
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 	char *code = "Good news, everyone!";
 
 	float vertices[] = {};
 
-	unsigned int indices[] = {  // note that we start from 0!
-		// snake head
-		0, 1, 3,  // first Triangle
-		1, 2, 3,   // second Triangle
-	};
-
 	// step one create teh buffer objects
-	GLuint vertex_array_object;
-	GLuint vertex_buffer_object;
-	GLuint element_buffer_object;
 
 	glGenVertexArrays(1, &vertex_array_object);
 	glGenBuffers(1, &vertex_buffer_object);
@@ -332,13 +331,16 @@ int main() {
 
 	int quit = 0;
 
+	float sx = 2.0 / SCREEN_WIDTH;
+	float sy = 2.0 / SCREEN_HEIGHT;
+
 	while (!quit) {
 		glfwPollEvents();
 
-		/* glDisable(GL_CULL_FACE); */
-		/* glDisable(GL_BLEND); */
+		glDisable(GL_CULL_FACE);
+		glDisable(GL_BLEND);
 
-		const GLfloat bgColor[] = {0.2f,0.3f,0.5f,1.0f};
+		const GLfloat bgColor[] = {0.10f,0.10f,0.10f,1.0f};
 		glClearBufferfv(GL_COLOR, 0, bgColor);
 
 		glUseProgram(texture_program);
@@ -348,19 +350,76 @@ int main() {
 
 		glBindTextureUnit(0, textureID);
 		glBindVertexArray(vertex_array_object);
-
 		glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object);
 
-		float x = img_height + 400;
-		float y = img_width + 400;
+		float x = w;
+		float y = SCREEN_HEIGHT;
+
+		Character ch = c[69];
+	/* int xoffset; // x offset in texture */
+	/* int yoffset; // y offset in texture */
+
+	/* float ax;	// advance.x */
+	/* float ay;	// advance.y */
+
+	/* float bw;	// bitmap.width; */
+	/* float bh;	// bitmap.height; */
+
+	/* float bl;	// bitmap_left; */
+	/* float bt;	// bitmap_top; */
+
+	/* float tx;	// x offset of glyph in texture coordinates */
+	/* float ty;	// y offset of glyph in texture coordinates */
+
+		printf("bw %f\n", ch.bw);
+		printf("bh  %f\n", ch.bh);
+		printf("ax %f\n", ch.ax);
+		printf("ay  %f\n", ch.ay);
+		printf("bl %f\n", ch.bl);
+		printf("bt  %f\n", ch.bt);
+		printf("yoff %d\n", ch.yoffset);
+		printf("xoff %d\n", ch.xoffset);
+
+		float spritewidth = ch.bw;
+		float spriteheight = ch.bh;
+
+		int yl= ch.yoffset;
+		int xl= ch.xoffset;
+
+
+		float blx = ((xl* spritewidth) / w);
+		float bly = ((yl* spriteheight) / h); // bl
+		float brx = (((1+xl) * spritewidth) / w);
+		float bry = ((yl * spriteheight) / h); // br
+		float trx = (((1+xl) * spritewidth) / w);
+		float try = (((1+yl) * spriteheight) / h); // tr
+		float tlx = ((xl * spritewidth) / w);
+		float tly = (((1+yl) * spriteheight) / h); //tl
 
 		float vertices[] = {
-			// snake
-			x - img_width, y, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f,// top left
+			// pos
+			x - spritewidth, y, 0.0f,
+			// color
+			1.0f, 0.0f, 0.0f, 1.0f, 
+			// tex coords
+			blx, bly, // bottom left
+			// tex index
+			0.0f,// top left
 
-			x, y, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f,// top right
-			x,  y - img_height, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, // bottom right
-			x - img_width, y - img_height, 0.0f,  1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f// bottom left
+			x, y, 0.0f, 
+			1.0f, 0.0f, 0.0f, 1.0f,
+			brx, bry, // bottom right
+			0.0f,// top right
+
+			x,  y - spriteheight, 0.0f, 
+			1.0f, 0.0f, 0.0f, 1.0f, 
+			trx, try,  // top right
+			0.0f, // bottom right
+
+			x - spritewidth, y - spriteheight, 0.0f,  
+			1.0f, 0.0f, 0.0f, 1.0f, 
+			tlx, tly,  // top left
+			0.0f// bottom left
 		};
 
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
@@ -379,15 +438,16 @@ int main() {
 
 		glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
 
+
 		/* glEnable(GL_CULL_FACE); */
 		/* glEnable(GL_BLEND); */
 		/* glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); */
 
-		/* float text_padding_left = 10.0f; */
-		/* float text_y_coordinates = (float)(SCREEN_HEIGHT) - 18; */
+		float text_padding_left = SCREEN_WIDTH / 2;
+		float text_y_coordinates = SCREEN_HEIGHT / 2;
 
 		/* render_text( */
-		/* 		code, */ 
+		/* 		"A", */ 
 		/* 		text_padding_left, */ 
 		/* 		text_y_coordinates, */
 		/* 		1.0f */
@@ -397,54 +457,93 @@ int main() {
 	}
 
 	/* glDeleteVertexArrays(1, &vertex_array_object); */
-	/* glDeleteProgram(program); */
+	glDeleteProgram(texture_program);
+	glDeleteProgram(font_program);
 	glfwTerminate();
 }
 
 void render_text(const char *text, float x, float y, float scale)
 {
-	glUseProgram(font_program);
+	glUseProgram(texture_program);
 
 	// font color
-	float r = 0.0f;
-	float g = 0.0f;
-	float b = 0.0f;
+	float r = 1.0f;
+	float g = 1.0f;
+	float b = 1.0f;
 
-	glUniform3f(glGetUniformLocation(font_program, "textColor"), r, g, b);
-	glActiveTexture(GL_TEXTURE0);
+	int loc = glGetUniformLocation(texture_program, "u_Textures");
+	int samplers[1] = {0};
+	glUniform1iv(loc, 1, samplers);
+
+	glBindTexture(GL_TEXTURE_2D, textureID);
 	glBindVertexArray(VAO);
 
 	float xpos = 0.0f;
 	float ypos = 0.0f;
 
-	for (unsigned int c = 0; c != strlen(text); c++) 
+	for (unsigned int i = 0; i < strlen(text); i++) 
 	{
-		Character ch = Characters[(int)text[c]];
-		xpos = x + ch.Bearing.x * scale;
-		ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+		Character ch = c[(int)text[i]];
+		xpos = x + ch.bl * scale;
+		ypos = y - (ch.bw - ch.bt) * scale;
 
-		float w = ch.Size.x * scale;
-		float h = ch.Size.y * scale;
-		float vertices[6][4] = {
-			{ xpos,     ypos + h,   0.0f, 0.0f },            
-			{ xpos,     ypos,       0.0f, 1.0f },
-			{ xpos + w, ypos,       1.0f, 1.0f },
+			/* VEC2 size = {face->glyph->bitmap.width, face->glyph->bitmap.rows}; */
+			/* VEC2 bearing = {face->glyph->bitmap_left, face->glyph->bitmap_top}; */
+	/* struct { */
+		/* 	float ax;	// advance.x */
+		/* 	float ay;	// advance.y */
 
-			{ xpos,     ypos + h,   0.0f, 0.0f },
-			{ xpos + w, ypos,       1.0f, 1.0f },
-			{ xpos + w, ypos + h,   1.0f, 0.0f }           
+		/* 	float bw;	// bitmap.width; */
+		/* 	float bh;	// bitmap.height; */
+
+		/* 	float bl;	// bitmap_left; */
+		/* 	float bt;	// bitmap_top; */
+
+		/* 	float tx;	// x offset of glyph in texture coordinates */
+		/* 	float ty;	// y offset of glyph in texture coordinates */
+		/* } c[128]; */
+		float w = ch.bw * scale;
+		float h = ch.bh * scale;
+		/* float vertices[6][4] = { */
+		/* 	{ xpos,     ypos + h,   0.0f, 0.0f }, */            
+		/* 	{ xpos,     ypos,       0.0f, 1.0f }, */
+		/* 	{ xpos + w, ypos,       1.0f, 1.0f }, */
+
+		/* 	{ xpos,     ypos + h,   0.0f, 0.0f }, */
+		/* 	{ xpos + w, ypos,       1.0f, 1.0f }, */
+		/* 	{ xpos + w, ypos + h,   1.0f, 0.0f } */           
+		/* }; */
+		float vertices[] = {
+			// snake
+			xpos, ypos+h, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f,// top left
+
+			x, y, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f,// top right
+			x+w,  ypos, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, // bottom right
+			x+w, y+h, 0.0f,  1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f// bottom left
 		};
 
-		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
 
+		glBufferData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // be sure to use glBufferSubData and not glBufferData
+
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, (sizeof(float)*10), (void *)0);
+
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, (sizeof(float)*10), (void *)12);
+
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, (sizeof(float)*10), (void *)28);
+
+		glEnableVertexAttribArray(3);
+		glVertexAttribPointer(3, 1, GL_FLOAT, GL_FALSE, (sizeof(float)*10), (void *)36);
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
 
-		x += (ch.Advance >> 6) * scale;
+		x += ((int)ch.ax >> 6) * scale;
+
 	}
 
+	/* glDrawElements(GL_TRIANGLES, 0, 6); */
+	glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
 	glBindVertexArray(0);
 	glBindTexture(GL_TEXTURE_2D, 0);
 }
